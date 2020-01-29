755dbce8caab0b7d5c80f2c551f07820cf97df36
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2020-01-29 19:00:41 +0100

    Made it work Multiworker

--- tf_distributed.py
+++ tf_distributed.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+# As recommended in https://www.tensorflow.org/tutorials/distribute/multi_worker_with_keras#train_the_model_with_multiworkermirroredstrategy
+mirrored_strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
+
+
 import os
 import json
 from dask_tensorflow import start_tensorflow
@@ -17,6 +22,8 @@ def scale_to_sge(n_workers):
     cluster.scale_up(n_workers)
     return Client(cluster)
 
+
+
 ## Dask using to boostrap the workers
 
 #cluster = LocalCluster(nanny=False, processes=False, n_workers=1, threads_per_worker=1, host="localhost", protocol="tcp://")
@@ -25,16 +32,25 @@ def scale_to_sge(n_workers):
 
 
 #### HERE WE NEED TO WAIT TO GET THE JOBS BEFORE GETTING THE SPEC
-import ipdb; ipdb.set_trace()
 client = scale_to_sge(2)
+import ipdb; ipdb.set_trace()
 
+
+from distributed.utils import sync
+#sync(client.loop, hook_tensorflow_server, client)
+#hook_tensorflow_server(client)
 
 ###### ONCE YOU GOT THE JOBS, CONTINUE
 
 
-tf_spec, dask_spec = start_tensorflow(client, ps=1, worker=1)
-#os.environ['TF_CONF'] = json.dumps(tf_spec)
-os.environ['TF_CONF'] = json.dumps(tf_spec.as_dict())
+tf_spec, dask_spec = start_tensorflow(client, ps=0, worker=2)
+
+os.environ['TF_CONFIG'] = json.dumps(tf_spec.as_dict())
+#base_dict = {}
+#base_dict["cluster"] = {}
+#base_dict["cluster"]["worker"] = ["localhost:2222"] + tf_spec.as_dict()["worker"] + tf_spec.as_dict()["ps"]
+#base_dict["task"] = {"type":"worker", "index":0}
+#os.environ['TF_CONFIG'] = json.dumps(base_dict)
 
 
 #tf_spec = {'ps': ['localhost:2222'], 'worker': ['localhost:2223'] }
@@ -57,7 +73,13 @@ def create_mnist_dataset():
 
     dataset = dataset.map(scale)
 
-    dataset = dataset.shuffle(2048).batch(64)
+    dataset = dataset.shuffle(10000).cache().batch(64)
+
+    options = tf.data.Options()
+    #https://www.tensorflow.org/api_docs/python/tf/data/experimental/AutoShardPolicy
+    options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
+    dataset = dataset.with_options(options)
+
 
     return dataset
     
@@ -66,27 +88,39 @@ class DummyModel(tf.keras.Model):
 
     def __init__(self):
         from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+        from tensorflow.keras import regularizers
+
         super(DummyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation='relu')
-        self.pool = MaxPool2D(pool_size=(2,2))
+        self.conv1 = Conv2D(32, 3, activation='relu', kernel_regularizer=regularizers.l2(0.01))
+        self.pool1 = MaxPool2D(pool_size=(2,2))
+
+        self.conv2 = Conv2D(64, 3, activation='relu', kernel_regularizer=regularizers.l2(0.01))
+        self.pool2 = MaxPool2D(pool_size=(2,2))
+
+
         self.flatten = Flatten()
-        self.d1 = Dense(128, activation='relu')
+        self.d1 = Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01))
         self.d2 = Dense(10, activation='softmax')
 
     def call(self, x):
         x = self.conv1(x)
-        x = self.pool(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
         x = self.flatten(x)
         x = self.d1(x)
 
         return self.d2(x)
 
 
+
 #### WRAPPING UP MY CODE INTO THE DISTRIBUTED STRATEGY
 
-mirrored_strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 #mirrored_strategy = tf.distribute.MirroredStrategy()
 with mirrored_strategy.scope():
+
+    #os.environ['TF_CONFIG'] = json.dumps(base_dict)
+
     model = DummyModel()
     dataset = create_mnist_dataset()
     
@@ -95,10 +129,10 @@ with mirrored_strategy.scope():
     
     loss_fn =  tf.nn.sparse_softmax_cross_entropy_with_logits
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.1)
+    optimizer = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.1)
 
 
-with mirrored_strategy.scope():
+    #os.environ['TF_CONFIG'] = json.dumps(base_dict)
 
     def train_replica_step(inputs, label):
         """
@@ -120,7 +154,7 @@ with mirrored_strategy.scope():
         grads = tape.gradient(loss, model.variables)
         optimizer.apply_gradients(zip(grads, model.variables))
 
-        tf.print("Replica Loss", loss)
+        #tf.print("Replica Loss", loss)
 
         return loss
 
